792af81017a0f8f22220836acdd151d2ae830436
Alexei Bobko <abvbobko@gmail.com>
2021-04-11 02:28:28 +0300

    Update test file and update camera calibrator (add input params and create more beautiful form)

--- creator_3d/reconstuctor/actions/camera_calibration.py
+++ creator_3d/reconstuctor/actions/camera_calibration.py
@@ -3,15 +3,22 @@ import PIL.Image
 import PIL.ExifTags
 import numpy as np
 
+# database of sensor sizes
+# https://github.com/openMVG/CameraSensorSizeDatabase
 
-# todo: посмотреть, может можно получать image path из картинки opencv
+# todo: посмотреть, может можно получать image path из картинки open cv
+# todo: можно сделать калибратор с клеточками (загружаешь фотку и оно калибрует по шахматам)
+
+DEFAULT_SENSOR_WIDTH = 1
+DEFAULT_SENSOR_HEIGHT = 1
+
 
 class Calibrator(Action):
     def __init__(self):
         self.__action_name = "Calibrator"
 
     @staticmethod
-    def get_exit_params(image):
+    def get_exif_params(image):
         """Return dict with image exif metadata.
 
         :param image: path to image on os
@@ -29,40 +36,148 @@ class Calibrator(Action):
         }
         return exif_data
 
-    def __get__camera_intrinsic_matrix_from_exif(self, image_path):
-        image = PIL.Image.open(image_path)
-        exif_params = self.get_exit_params(image)
+    @staticmethod
+    def get_image_size(image_path):
+        """Return image size in pixels
+
+        Args:
+            image_path (str): path to image
+
+        Returns:
+            (tuple(int, int)): image width and height in pixels.
+            if image does not exist - return (None, None).
+        """
+
+        try:
+            image = PIL.Image.open(image_path)
+        except (FileNotFoundError, AttributeError):
+            return None, None
+        return image.size
+
+    def get_intrinsic_matrix_from_exif(self, image_path):
+        try:
+            image = PIL.Image.open(image_path)
+        except (FileNotFoundError, AttributeError):
+            return None
+
+        exif_params = self.get_exif_params(image)
         if not exif_params:
             return None
+
         focal_length = exif_params['FocalLength']
         width, height = image.size
+        # todo: тут не так умножать надо скорее всего
+
         k = np.array([[float(focal_length*width), 0,                          width/2],
                       [0,                         float(focal_length*height), height/2],
                       [0,                         0,                          1]])
         return k
 
-    def __calibrate(self, image_path):
-        k = self.__get__camera_intrinsic_matrix_from_exif(image_path)
-        if k is not None:
-            return k
-        image = PIL.Image.open(image_path)
-        width, height = image.size
-        k = np.array([[float(3 * width), 0, width / 2],
-                      [0, float(3 * height), height / 2],
-                      [0, 0, 1]])
+    # def __calibrate(self, image_path):
+    #     # todo: change all method inside
+    #     k = self.get_intrinsic_matrix_from_exif(image_path)
+    #     if k is not None:
+    #         return k
+    #     image = PIL.Image.open(image_path)
+    #     width, height = image.size
+    #     k = np.array([[float(3 * width), 0, width / 2],
+    #                   [0, float(3 * height), height / 2],
+    #                   [0, 0, 1]])
+    #     return k
+    #     # todo: call __calibrate_with_F_matrix?
+
+    def get_intrinsic_matrix(self, **kwargs):
+        """Combine all provided parameters and
+        return intrinsic matrix according this parameters.
+
+        If camera parameters are provided - generate matrix
+        if image_path - try to get camera parameters from exif
+
+        Args:
+            **kwargs:
+                image_path (str): path to test image to get
+                    intrinsic camera parameters (optional)
+                f_mm (float): focal camera length in mm (optional)
+                image_width (int): width of image in pixels (optional)
+                image_height (int): height of image in pixels (optional)
+                sensor_width (int): width of sensor in mm (optional, has default value)
+                sensor_height (int): height of sensor in mm (optional, has default value)
+
+        Returns:
+            (np.array): intrinsic camera matrix (3x3)
+        """
+
+        f_mm = kwargs.get('f_mm')
+
+        im_width = kwargs.get('image_width')
+        im_height = kwargs.get('image_height')
+        sensor_width = kwargs.get('sensor_width', DEFAULT_SENSOR_WIDTH)
+        sensor_height = kwargs.get('sensor_height', DEFAULT_SENSOR_HEIGHT)
+        image_path = kwargs.get('image_path')
+
+        k = None
+
+        # if no image size - get size from test image
+        if (im_width and im_height) is None:
+            im_width, im_height = self.get_image_size(image_path)
+
+        # if we have all parameters - create K
+        if (f_mm and im_width and im_height and sensor_width and sensor_height) is not None:
+            k = self.create_intrinsic_matrix(f_mm=f_mm,
+                                             image_width=im_width, image_height=im_height,
+                                             sensor_width=sensor_width, sensor_height=sensor_height)
+        elif image_path:
+            # if we don't have some parameters - try to get K from test image
+            k = self.get_intrinsic_matrix_from_exif(image_path)
+
+        if not k:
+            # todo: calibrate
+            raise Exception("Couldn't get intrinsic camera matrix")
+    
         return k
-        # todo: call __calibrate_with_F_matrix?
+
+    @staticmethod
+    def create_intrinsic_matrix(f_mm, image_width, image_height, sensor_width=1, sensor_height=1):
+        """Return intrinsic camera matrix (K) by parameters
+
+        K is
+            | au 0  u0 |
+            | 0  av v0 |
+            | 0  0  1  |,
+
+            where au = f/(sensor width), av = f/(sensor height)
+                  u0, v0 is central point of the image frame (x and y, usually center of image).
+
+        Args:
+            f_mm (float): focal length in mm
+            image_width (int): image width in pixels
+            image_height (int): image height in pixels
+            sensor_width (float): width of one pixel in world coordinates (mm)
+            sensor_height (float): height of one pixel in world coordinates (mm)
+        Returns:
+            (np.array): intrinsic camera matrix (3x3)
+        """
+
+        return np.array([[float(f_mm/sensor_width), 0,                         image_width/2],
+                         [0,                        float(f_mm/sensor_height), image_height/2],
+                         [0,                        0,                         1]])
 
     def run(self, **kwargs):
-        image_path = kwargs['image_1_path']
-        return self.__calibrate(image_path)
+
+        f_mm = kwargs.get('f_mm')
+        image_width, image_height = kwargs.get('image_width'), kwargs.get('image_height')
+        sensor_width, sensor_height = kwargs.get('sensor_width', 1), kwargs.get('sensor_height', 1)
+        image_path = kwargs.get('image_path')
+
+        return self.get_intrinsic_matrix(f_mm=f_mm,
+                                         image_width=image_width, image_height=image_height,
+                                         sensor_width=sensor_width, sensor_height=sensor_height,
+                                         image_path=image_path)
 
     @property
     def action_name(self):
         return self.__action_name
 
-    # todo: чекать, есть ли EXIF данные
-        # todo: если есть - достать калибровку (K) камеры
-        # todo: если нету - откалибровать камеру
 
-    # todo: можно сделать калибратор с клеточками (загружаешь фотку и оно калибрует по шахматам)
+if __name__ == '__main__':
+    print(Calibrator.get_image_size('test/test.jpg'))
---
+++ creator_3d/test/main_test.py
@@ -0,0 +1,58 @@
+"""This test uses math ideas from computer vision course
+    and try to be like a final code
+"""
+
+import os
+
+from creator_3d.reconstuctor.actions import feature_extraction
+from creator_3d.reconstuctor.actions import feature_matching
+from creator_3d.reconstuctor.actions import geometric_verification
+from creator_3d.reconstuctor.actions import camera_calibration
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def get_file_list(data_path, ext, num_of_files=-1):
+    file_list = []
+    cnt = 0
+    for file in os.listdir(DATA_PATH):
+        if cnt == num_of_files:
+            break
+        if file.split(".")[-1] == extension:
+            file_list.append(file)
+    return file_list
+
+
+DATA_PATH = "C:\\Users\\hp\\Desktop\\3DCreator\\creator_3d\\data\\rock_head\\"
+extension = 'jpg'
+
+sift = feature_extraction.SIFT()
+matcher = feature_matching.BFMatcher()
+geometric_verifier = geometric_verification.PointForFMatrix()
+
+image_files_list = get_file_list(DATA_PATH, extension)
+num_of_image_files = len(image_files_list)
+
+# todo: передавать следующей картинке координаты текущей относительно первой
+# todo: то есть чтобы все было относительно первой картинки в итоге
+
+for i in range(num_of_image_files - 1):
+    image_1_path = os.path.join(DATA_PATH, image_files_list[i])
+    image_2_path = os.path.join(DATA_PATH, image_files_list[i+1])
+
+    image_1 = cv2.imread(image_1_path)
+    image_2 = cv2.imread(image_2_path)
+
+    key_points_1, descriptors_1 = sift.run(image=image_1)
+    key_points_2, descriptors_2 = sift.run(image=image_2)
+
+    good = matcher.match_features(key_points_1, descriptors_1, key_points_2, descriptors_2)
+
+    pts1, pts2, F = geometric_verifier.verify(good, image_1, image_2, key_points_1, key_points_2)
+
+    calibrator = camera_calibration.Calibrator()
+    k = calibrator.run(image_1_path=image_1_path)
+    print(k)
+
