d99c8996e8a05dc9183bc8eed4deedfb458f7d5c
Alexei Bobko <abvbobko@gmail.com>
2021-06-06 18:50:44 +0300

    Update win init

--- creator_3d/window/constants/const.py
+++ creator_3d/window/constants/const.py
@@ -3,3 +3,12 @@ import os
 UI_FOLDER = './window'
 UI_FILE_NAME = 'main_window.ui'
 UI_PATH = os.path.join(UI_FOLDER, UI_FILE_NAME)
+
+WIN_TITLE = "3DCreator"
+
+class IMAGE_TABLE:
+    table_header = ['Order', 'Name', 'Delete']
+
+    @staticmethod
+    def cols_count():
+        return len(IMAGE_TABLE.table_header)
--- creator_3d/window/main_window.py
+++ creator_3d/window/main_window.py
@@ -6,105 +6,130 @@ from PyQt5.QtCore import Qt
 import cv2
 import os
 import creator_3d.window.constants.const as window_const
+from creator_3d.controllers.main_controller import MainController
 
 
 class MainWindow(QMainWindow):
     def __init__(self, ui_path=window_const.UI_PATH):
-        super(MainWindow, self).__init__()
+
+        self.main_controller = MainController()
+
+        super().__init__()
 
         # init UI
         uic.loadUi(ui_path, self)
 
-        # add drag and drop mode to image table
-        self.image_table.setDragEnabled(True)
-        self.image_table.setAcceptDrops(True)
-        # self.image_table.viewport().setAcceptDrops(True)
-        # self.image_table.setDragDropOverwriteMode(False)
-        # self.image_table.setDropIndicatorShown(True)
+        # set window title
+        self.setWindowTitle(window_const.WIN_TITLE)
+
+        self.image_table.setColumnCount(window_const.IMAGE_TABLE.cols_count())
+        self.image_table.setHorizontalHeaderLabels(window_const.IMAGE_TABLE.table_header)
+
+
+        # todo: example
+        # test_text = [[str(1), "0001.jpg", None],
+        #              [str(2), "0000.jpg", None],
+        #              [str(3), "0002.jpg", None]]
         #
-        # self.image_table.setSelectionMode(QAbstractItemView.SingleSelection)
-        # self.image_table.setSelectionBehavior(QAbstractItemView.SelectRows)
-        # self.image_table.setDragDropMode(QAbstractItemView.InternalMove)
 
-        # add some test data to table
-        self.image_table.setColumnCount(3)
-        self.image_table.setRowCount(3)
+        # self.image_table.setRowCount(3)
 
-        self.image_table.setHorizontalHeaderLabels(['Order', 'Name', 'Delete'])
-
-        test_text = [[str(1), "0001.jpg", None],
-                     [str(2), "0000.jpg", None],
-                     [str(3), "0002.jpg", None]]
-
+        # set width of column using size of text in header
         header = self.image_table.horizontalHeader()
         header.setSectionResizeMode(0, QHeaderView.Stretch)
         header.setSectionResizeMode(1, QHeaderView.ResizeToContents)
         header.setSectionResizeMode(2, QHeaderView.ResizeToContents)
 
-        for i in range(len(test_text)):
-            for j in range(len(test_text[i])):
-                if test_text[i][j]:
-                    item = QTableWidgetItem(test_text[i][j])
-                    self.image_table.setItem(i, j, item)
-                else:
-                    btn_sell = QPushButton('X')
-                    # btn_sell.clicked.connect(self.handleButtonClicked)
-                    self.image_table.setCellWidget(i, j, btn_sell)
-
         # for i in range(len(test_text)):
-        #     item = QTableWidgetItem(test_text[i])
-        #     # item.setFlags(item.flags() | Qt.ItemIsSelectable)
-        #     self.image_table.setItem(i, 0, item)
-
-        # test for 6 chapter
-        self.feature_extraction_combobox.clear()
-        self.feature_extraction_combobox.addItems(['SIFT', 'SURF', 'ORB'])
-        self.feature_matching_combobox.clear()
-        self.feature_matching_combobox.addItems(['FLANN', 'BF', 'KNN'])
-        self.reconstruction_combobox.clear()
-        self.reconstruction_combobox.addItems(['Reconstructor'])
-        self.bundle_adjustment_combobox.clear()
-        self.bundle_adjustment_combobox.addItems(['Bundle adjustment'])
-
-        text = [["Feature extraction", "SIFT", "nfeatures", "nOctaveLayers", "contrastThreshold", "edgeThreshold",
-                 "sigma"],
-                ["", "SIFT", str(0), str(3), str(0.04), str(10), str(1.6)],
-                ["Feature matching", "BF", "normType", "crossCheck", "MRT", "", ""],
-                ["", "BF", "NORM_L2", "FALSE", "0.7", "", ""],
-                ["Reconstruction", "Reconstructor", "E_prob", "E_threshold", "", "", ""],
-                ["", "Reconstructor", "0.999", "1", "", "", ""],
-                ["Bundle adjustment", "Bundle adjustment", "x_threshold", "y_threshold", "", "", ""],
-                ["", "Bundle adjustment", "0.5", "1", "", "", ""]]
-        self.algorithm_parameters_table.setColumnCount(len(text[0]))
-        self.algorithm_parameters_table.setRowCount(len(text))
-        self.algorithm_parameters_table.setHorizontalHeaderLabels(['Step', 'Algorithm', 'param_1', 'param_2', 'param_3',
-                                                                   'param_4', 'param_5'])
-
-        header = self.algorithm_parameters_table.horizontalHeader()
-        header.setSectionResizeMode(0, QHeaderView.Stretch)
-        for i in range(1, 7):
-            header.setSectionResizeMode(i, QHeaderView.ResizeToContents)
-
-        for i in range(len(text)):
-            for j in range(len(text[i])):
-                item = QTableWidgetItem(text[i][j])
-                self.algorithm_parameters_table.setItem(i, j, item)
+        #     for j in range(len(test_text[i])):
+        #         if test_text[i][j]:
+        #             item = QTableWidgetItem(test_text[i][j])
+        #             self.image_table.setItem(i, j, item)
+        #         else:
+        #             btn_sell = QPushButton('X')
+        #             # btn_sell.clicked.connect(self.handleButtonClicked)
+        #             self.image_table.setCellWidget(i, j, btn_sell)
+        #
+        # # for i in range(len(test_text)):
+        # #     item = QTableWidgetItem(test_text[i])
+        # #     # item.setFlags(item.flags() | Qt.ItemIsSelectable)
+        # #     self.image_table.setItem(i, 0, item)
+        #
+        # # test for 6 chapter
+        # self.feature_extraction_combobox.clear()
+        # self.feature_extraction_combobox.addItems(['SIFT', 'SURF', 'ORB'])
+        # self.feature_matching_combobox.clear()
+        # self.feature_matching_combobox.addItems(['FLANN', 'BF', 'KNN'])
+        # self.reconstruction_combobox.clear()
+        # self.reconstruction_combobox.addItems(['Reconstructor'])
+        # self.bundle_adjustment_combobox.clear()
+        # self.bundle_adjustment_combobox.addItems(['Bundle adjustment'])
+        #
+        # text = [["Feature extraction", "SIFT", "nfeatures", "nOctaveLayers", "contrastThreshold", "edgeThreshold",
+        #          "sigma"],
+        #         ["", "SIFT", str(0), str(3), str(0.04), str(10), str(1.6)],
+        #         ["Feature matching", "BF", "normType", "crossCheck", "MRT", "", ""],
+        #         ["", "BF", "NORM_L2", "FALSE", "0.7", "", ""],
+        #         ["Reconstruction", "Reconstructor", "E_prob", "E_threshold", "", "", ""],
+        #         ["", "Reconstructor", "0.999", "1", "", "", ""],
+        #         ["Bundle adjustment", "Bundle adjustment", "x_threshold", "y_threshold", "", "", ""],
+        #         ["", "Bundle adjustment", "0.5", "1", "", "", ""]]
+        # self.algorithm_parameters_table.setColumnCount(len(text[0]))
+        # self.algorithm_parameters_table.setRowCount(len(text))
+        # self.algorithm_parameters_table.setHorizontalHeaderLabels(['Step', 'Algorithm', 'param_1', 'param_2', 'param_3',
+        #                                                            'param_4', 'param_5'])
+        #
+        # header = self.algorithm_parameters_table.horizontalHeader()
+        # header.setSectionResizeMode(0, QHeaderView.Stretch)
+        # for i in range(1, 7):
+        #     header.setSectionResizeMode(i, QHeaderView.ResizeToContents)
+        #
+        # for i in range(len(text)):
+        #     for j in range(len(text[i])):
+        #         item = QTableWidgetItem(text[i][j])
+        #         self.algorithm_parameters_table.setItem(i, j, item)
 
         # test_text = ["text_1", "text_2", "text_3"]
         # for i in range(len(test_text)):
         #     # item.setFlags(item.flags() | Qt.ItemIsSelectable)
         #
 
+    def fill_image_table(self, text_values):
+        pass
+
+    def remove_item_from_image_table(self):
+        pass
+
+    def get_next_image_number(self):
+        pass
+
+    def load_image(self):
+        pass
+
+    def load_images(self):
+        pass
+
+    def change_order_in_image_table(self):
+        pass
+
+    def init_combobox(self):
+        pass
+
+    def set_default_algorithm(self):
+        pass
+
+    # error window
+    # message window
+    # show_model
+    #
+
     # todo: Main
-    # todo: add extension of existing widgets
     # todo: add parameters setting
     # todo: add algorithm choosing by combobox
     # todo: may be buttons add "Add images" and "Clear" instead of "Load images"
     # todo: add camera params input (f, sw, sh, get_camera_params)
 
     # todo: QTableWidget
-    # todo: extend QTableWidget
-    # todo: add drag and drop to QTableWidget
     # todo: add deleting from file list
 
     # todo: Load button
